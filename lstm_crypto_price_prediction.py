83544976216f2ece6f4c4537d9be90c6059a1c56
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-08-13 06:29:08 +0200

    Update lstm_crypto_price_prediction.py

--- lstm_crypto_price_prediction.py
+++ lstm_crypto_price_prediction.py
@@ -1,13 +1,7 @@
-# Beware troglodytes! Utilizing this tool for the sole purpose 
-# of forecasting asset prices and subsequently participating 
-# in speculative ventures is strongly discouraged. At 
-# best, this may serve as a supplementary indicator within 
-# your broader investment strategy.
-# -cbrwx - https://github.com/cbrwx
-
-# ---------------------------------
-
 # Importing necessary libraries
+import ccxt
+import hmac
+import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
@@ -15,8 +9,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Fallback for TensorFlow logging
+logging.getLogger('tensorflow').setLevel(logging.FATAL)  # Python logging for TensorFlow
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+import time
 
 # Configuring matplotlib styling
 plt.style.use('dark_background')
@@ -26,11 +24,33 @@ plt.rcParams['axes.edgecolor'] = 'white'
 plt.rcParams['grid.color'] = 'white'
 plt.rcParams['text.color'] = 'white'
 
-# Read the CSV file, using data from yahoo, so it needs to feature data into a time series dataset
-df = pd.read_csv('optionalurlorfilenamedirectlyfromurdrive.csv', header=0, parse_dates=['Date'])
 
-# Sort the data by the 'Date' column
-df = df.sort_values(by='Date')
+# Input your Gate.io API(v2) credentials below (works as of 13.08.2023)
+# ccxt lib works on most exchanges, so fcuk around and find out
+exchange = ccxt.gateio({
+   'apiKey': 'yourkey',
+   'secret': 'yourfancysecret',
+})
+
+# Prompt the user for the asset, period, and interval
+asset = input("Enter the ticker of the asset: ")
+eepochs = int(input("Epochs: "))
+print("The 'pred_duration' represents the number of future intervals for which the model will make predictions. This value will be multiplied by the chosen data interval. For example, if the data interval is 1 hour and 'pred_duration' is 25, the model will predict the next 25 hours. If the data interval is 1 day, it will predict the next 25 days, and so on.")
+pred_duration, look_back = map(int, input("Enter the prediction duration and look back value factor, separated by a comma: ").split(','))
+interval = input("Enter the data interval (1m,5m,15m,30m,1h,4h,1d): ")
+period = int(input("Enter the period in minutes (value should be larger than interval(in minutes)): "))
+
+def fetch_data(exchange, asset, interval, period):
+    current_time = exchange.milliseconds()
+    data_start_time = current_time - period * 60 * 1000  # Convert period from minutes to milliseconds
+    ohlcv_data = exchange.fetch_ohlcv(asset + '_USDT', interval, data_start_time)
+    df = pd.DataFrame(ohlcv_data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='ms')
+    df.set_index('Timestamp', inplace=True)
+    return df
+
+# Fetch the data
+df = fetch_data(exchange, asset, interval, period)
 
 # Select the 'Close', 'Open', 'High', 'Low', and 'Volume' columns as the features to be predicted
 feature = df[['Close', 'Open', 'High', 'Low', 'Volume']]
@@ -53,11 +73,10 @@ def create_dataset(dataset, look_back=1):
         dataY.append(dataset[i + look_back])
     return np.array(dataX), np.array(dataY)
 
-# Define the look_back value and convert the feature data into time series dataset
-look_back = 30
-X_train, y_train = create_dataset(train_feature, 30)
-X_val, y_val = create_dataset(val_feature, 30)
-X_test, y_test = create_dataset(test_feature, 30)
+# Define the look_back value and convert the feature data into the series dataset
+X_train, y_train = create_dataset(train_feature, look_back)
+X_val, y_val = create_dataset(val_feature, look_back)
+X_test, y_test = create_dataset(test_feature, look_back)
 
 # Reshape the data for LSTM model
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 5))
@@ -73,20 +92,20 @@ model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam())
 
 # Fit the model to the training data (edit for optimization of your model)
-model.fit(X_train, y_train, epochs=55, batch_size=1, verbose=3)
+model.fit(X_train, y_train, epochs=eepochs, batch_size=1, verbose=3)
 
-# Make predictions for the next 30 days
+# Make predictions for the next 'pred_duration' days
 predictions = []
-for i in range(25):
+for i in range(pred_duration):
     # predict the next day's closing price
     yhat = model.predict(X_test[i].reshape(1, -1, 5))[0]
     predictions.append(yhat)
     # update the input data for the next prediction
-    if i < 29:
+    if i < pred_duration-1:
         yhat = np.reshape(yhat, (1, 1, yhat.shape[-1]))
-        if i < X_test.shape[0]-1:
-            X_test[i+1, :-1, :] = X_test[i, 1:, :]
-            X_test[i+1, -1, :] = yhat
+        if i < X_test.shape[0] - 1:
+            X_test[i + 1, :-1, :] = X_test[i, 1:, :]
+            X_test[i + 1, -1, :] = yhat
 
 # Plot the predictions
 fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(2000/80, 400/80), dpi=80)
